534c12384a1b0bcb61b069d76aeca5c99b68d645
nija0225 <nikhita.qa@gmail.com>
2025-05-15 10:09:45 +0530

    Ready to go

--- src/main/java/com/testing/framework/endpoints/APIConstants.java
+++ src/main/java/com/testing/framework/endpoints/APIConstants.java
@@ -1,4 +1,9 @@
 package com.testing.framework.endpoints;
 
 public class APIConstants {
+    public static String base_URL = "https://restful-booker.herokuapp.com/";
+    // public static String BASE_URL = // Fetch this from Excel file
+    public static String auth = "/auth";
+    public static String createBooking = "/booking";
+    public static String PING_URL = "/ping";
 }
